c31283e2ab588b763e55b8089c2874bfb66e2d17
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-16 18:19:40 +0530

    code modified for storing users

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 //init google oauth - passport - authentication 
+require('./models/User')
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+
 //mongo database connect
-mongoose.connect(require('./config/keys').mongoURI);
+
 
 //google oauth routes
 require('./routes/authRoutes')(app);
 
 // port
 const PORT = 5000 || process.env.PORT;
-
 
 app.listen(PORT);
