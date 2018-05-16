140df83373551decc38ad04baca37ddd28d5b7fc
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-16 16:04:21 +0530

    added mongoDb client

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+
+//init google oauth - passport - authentication 
+require('./services/passport');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+//mongo database connect
+mongoose.connect(require('./config/keys').mongoURI);
 
+//google oauth routes
+require('./routes/authRoutes')(app);
+
+// port
 const PORT = 5000 || process.env.PORT;
+
 
 app.listen(PORT);
