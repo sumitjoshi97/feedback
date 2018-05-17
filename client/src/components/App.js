e73ae7d4b6c4f9ec6d93a4cd4485a4fa13aa9e05
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-17 12:56:08 +0530

    added components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                react
-            </div>
-        )
-    }
-}
+// importing react router
+import {Route, Switch} from 'react-router-dom';
+
+// importing component
+import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
+import Survey from './Survey';
+
+const App = () => (
+    <div>
+        <Header/>
+        <Switch>
+            <Route path='/' component={Landing}/>
+            <Route path='/dashboard' component={Dashboard}/>
+            <Route path='/survey' component={Survey}/>
+        </Switch>
+    </div>
+)
 
 export default App;
