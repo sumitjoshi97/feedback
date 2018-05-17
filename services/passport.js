72c23250120d172a2e2496a35a5df5f93f6e538f
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-17 11:23:31 +0530

    modified promises to aync await

--- services/passport.js
+++ services/passport.js
@@ -17,24 +17,17 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({
-                googleId: profile.id
-            })
-            .then(existingUser => {
-                if (existingUser) {
-                    //we have record
-                    done(null, existingUser);
-                } else {
-                    new User({
-                            googleId: profile.id
-                        })
-                        .save()
-                        .then(user => done(null, user))
-                }
-            });
-    }));
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId: profile.id})
+
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+        const user = await new User({googleId: profile.id}).save();
+        done(null, user);
+    })
+);
