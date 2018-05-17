b716a14e36b54346981e29d5b7ee37d9494b85ae
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-17 12:39:00 +0530

    code refactored

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
+
+import {createStore, applyMiddleware} from 'redux';
+import { BrowserRouter as Router } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(() => [], {}, applyMiddleware)
+
+const app = (
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
