6445fc96443ac0f248ab3ebde8659d0695f79e3b
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-19 23:45:53 +0530

    redux form added

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux';
 
 import authReducer from './authReducer';
+import {reducer as formReducer} from 'redux-form';
 
 const rootReducer =  combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 })
 
 export default rootReducer;
