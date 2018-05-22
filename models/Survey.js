1482874b3ac72ac96d08286757d13ff388392452
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-22 22:38:09 +0530

    typo corrected

--- models/Survey.js
+++ models/Survey.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const RecepientSchema = require('./Recepient');
 
-const surveySchema = Schema({
+const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
-    recepients: [RecepientSchema],
+    recipients: [RecepientSchema],
     yes: { type: Number, default: 0 },
     no: { type: Number, default: 0 }, 
     _user: { type: Schema.Types.ObjectId, ref: 'User' },
