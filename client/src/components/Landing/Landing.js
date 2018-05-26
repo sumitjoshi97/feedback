e47823f871f7f04bd262a24fca36d59f5ba927f3
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-26 23:52:27 +0530

    surveys button added

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,39 +1,50 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './Landing.css';
 import landingImg from '../../assets/img/feedback-large.jpeg';
 
-const Landing = (props) => (
-    <div className="landing">
-        <div className="landing-content">
-            <h1 className="landing-content__heading">
-                Emaily
-            </h1>
-            <div className="landing-content__text">
-                <p className="landing-content__para">
-                    Collect feedback from your users
-                </p>
-                <ul className="landing-content__list">
-                    <li>
-                        Create surveys
-                    </li>
-                    <li>
-                        Send surveys via email
-                    </li>
-                    <li>
-                        Check surveys
-                    </li>
-                    <li>
-                        Delete surveys
-                    </li>
-                </ul>
+const Landing = (props) => {
+
+    return (
+        <div className="landing">
+            <div className="landing-content">
+                <h1 className="landing-content__heading">
+                    Emaily
+                </h1>
+                <div className="landing-content__text">
+                    <p className="landing-content__para">
+                        Collect feedback from your users
+                    </p>
+                    <ul className="landing-content__list">
+                        <li>
+                            Create surveys
+                        </li>
+                        <li>
+                            Send surveys via email
+                        </li>
+                        <li>
+                            Check surveys
+                        </li>
+                        <li>
+                            Delete surveys
+                        </li>
+                    </ul>
+                </div>
+
+                {props.auth ? <Link to="/surveys" className="btn-inline">Surveys</Link> : <a className="btn-inline" href="/auth/google">Login</a>}
+                {/* <Link to="/surveys" className="btn-inline">Surveys</Link> */}
             </div>
 
+            <img src={landingImg} alt="landing-image" className="landing-image"/>
+
         </div>
+    )
+}
 
-        <img src={landingImg} alt="landing-image" className="landing-image"/>
+const mapStateToProps = state => {
+    return {auth: state.auth.auth}
+}
 
-    </div>
-)
-
-export default Landing;
+export default connect(mapStateToProps)(Landing);
