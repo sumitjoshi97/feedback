1285dc263a9c19914972f9989aeead37fc0e3f81
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-26 16:57:59 +0530

    markup updated

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
+import './Landing.css';
+import landingImg from '../../assets/img/feedback-large.jpeg';
+
 const Landing = (props) => (
-    <div>
-        <h1>
-            Emaily
-        </h1>
-        Collect feedback frmm your users
+    <div className="landing">
+        <div className="landing-content">
+            <h1 className="landing-content__heading">
+                Emaily
+            </h1>
+            <div className="landing-content__text">
+                <p className="landing-content__para">
+                    Collect feedback from your users
+                </p>
+                <ul className="landing-content__list">
+                    <li>
+                        Create surveys
+                    </li>
+                    <li>
+                        Send surveys via email
+                    </li>
+                    <li>
+                        Check surveys
+                    </li>
+                    <li>
+                        Delete surveys
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+        <img src={landingImg} alt="landing-image" className="landing-image"/>
+
     </div>
 )
 
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
     render() {
         return (
-            <div className="container">
+            <div >
                 <Router>    
                     <div>
                     <Header/>
