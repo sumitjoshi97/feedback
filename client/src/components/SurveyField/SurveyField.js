2f62fb1419916bbbaae0410bcb83df631df81f66
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-26 23:53:50 +0530

    survey fileld styling added

--- client/src/components/SurveyField/SurveyField.js
+++ client/src/components/SurveyField/SurveyField.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import './SurveyField.css';
 
 const SurveyField = ({input, label, meta: {error, touched}}) => (
-    <div>
-        <label>{label}</label>
-        <input {...input}/>
+    <div className="form-field">
+        <label className="form-label">{label}</label>
+        <input {...input} className="form-input"/>
         <span style={{color: 'red', fontSize: '10px', marginTop: '-40px'}}>{touched ? error : null}</span>
     </div>
 );
