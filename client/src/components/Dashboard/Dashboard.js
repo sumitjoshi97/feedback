4b5c6ab5c994914602406b6b9c670eae26d36969
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-26 23:51:30 +0530

    styles for dashboard added

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,17 +2,20 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import SurveyList from '../../containers/Surveys/SurveyList/SurveyList';
+import './Dashboard.css';
+import AddSvg from '../../assets/svg/add-song.svg'
 
 const Dashboard = () => {
     return (
-        <div>
+        <div className="dashboard">
             <SurveyList/>
 
-            <div className="fixed-action-btn">
-                <Link to="/surveys/new" className="btn-floating btn-large red">
-                    <i className="material-icons">add</i>
-                </Link>
-            </div>
+            <Link to="/surveys/new" className="btn-fixed">
+                {/* <svg class="side-nav__icon">
+                    {AddSvg}
+                </svg> */}
+                <img src={AddSvg} alt=""/>
+            </Link>
         </div>
     )
 }
