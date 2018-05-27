9493471edfd339ea0517a706c93649df904c0338
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-05-27 17:20:25 +0530

    refactored

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 
 import './index.css';
-// import 'materialize-css/dist/css/materialize.min.css';
 
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux'
-import { BrowserRouter as Router } from "react-router-dom";
 import thunk from 'redux-thunk';
 import rootReducer from './store/reducers/index';
 
