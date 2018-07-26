20314a3fa3ccf5fe49f1a5304126f0e4a86f933a
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-07-26 15:26:31 +0530

    typo changed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
-require('./models/User');
+require('./models/user');
 require('./models/Survey');
 require('./services/passport');
 
