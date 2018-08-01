364d00d19aea553bbe613ef4d1f52ef1d60a8cc1
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-01 23:28:40 +0530

    secure cookie settings added

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
+    keys: [keys.cookieKey],
+    secureProxy: true
   })
 );
 app.use(passport.initialize());
