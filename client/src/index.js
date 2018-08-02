aba91bcb24cd925ac5e05e11614d196f907da540
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-02 14:01:22 +0530

    service worker removed

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,6 @@ import {Provider} from 'react-redux'
 import thunk from 'redux-thunk';
 import rootReducer from './store/reducers/index';
 
-import registerServiceWorker from './registerServiceWorker';
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
@@ -21,4 +19,3 @@ const app = (
 );
 
 ReactDOM.render(app, document.getElementById('root'));
-registerServiceWorker();
