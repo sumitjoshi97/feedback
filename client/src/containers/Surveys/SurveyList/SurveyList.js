14dab251bb9fe024c8d9d6fa66f9998caf37557e
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-14 20:11:31 +0530

    added functionality of survey summary on clicking survey

--- client/src/containers/Surveys/SurveyList/SurveyList.js
+++ client/src/containers/Surveys/SurveyList/SurveyList.js
@@ -1,29 +1,35 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux';
-
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom'
 import * as actions from '../../../store/actions/index';
 import './SurveyList.css';
 
 class SurveyList extends Component {
 
-    componentDidMount = () =>{
+    componentDidMount = () => {
         this.props.onFetchSurveys();
     }
 
+    handleSurveyClick = survey => {
+        this.props.onSetCurrentSurvey(survey)
+        
+        this.props.history.push('/surveys/survey')
+    }
     renderSurveys = () => {
-        return this.props.surveys.reverse().map(survey=> {
-            return(
-            <div className="survey" key={survey._id}>
-                <div className="survey__content">
-                    <span className="survey__heading">{survey.title}</span>
-                    <p>{survey.body}</p>                         
-                    <p className="survey__date">Sent on: <span>{new Date(survey.dateSent).toLocaleDateString()}</span></p>                     
-                </div>                     
-                <div className="survey__actions">
-                    <a>Yes: {survey.yes}</a>
-                    <a>No: {survey.no}</a>                     
+        return this.props.surveys.reverse().map(survey => {
+            return (
+                <div className="survey" key={survey._id} onClick={() => this.handleSurveyClick(survey)}>
+                    <div className="survey__content">
+                        <span className="survey__heading">{survey.title}</span>
+                        <p>{survey.body}</p>
+                        <p className="survey__date">Sent on: <span>{new Date(survey.dateSent).toLocaleDateString()}</span></p>
+                    </div>
+                    <div className="survey__actions">
+                        <a>Yes: {survey.yes}</a>
+                        <a>No: {survey.no}</a>
+                    </div>
                 </div>
-            </div>)
+            )
         });
     }
 
@@ -42,14 +48,16 @@ class SurveyList extends Component {
 
 const mapStateToProps = state => {
     return {
-        surveys: state.surveys.surveys
+        surveys: state.surveys.surveys,
+        currentSurvey: state.surveys.currentSurvey
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchSurveys: () => dispatch(actions.fetchSurveysApi())
+        onFetchSurveys: () => dispatch(actions.fetchSurveysApi()),
+        onSetCurrentSurvey: (survey) => dispatch(actions.setCurrentSurvey(survey))
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(SurveyList);
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(SurveyList));
