dd4157f2b84d8bb359373675020a271b45d00f13
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-14 20:08:27 +0530

    survey reducer updated with setCurrentSurvey

--- client/src/store/reducers/surveyReducer.js
+++ client/src/store/reducers/surveyReducer.js
@@ -1,16 +1,24 @@
 import * as actionTypes from '../actions/types';
 
 const initialState = {
-    surveys: []
+    surveys: [],
+    currentSurvey: null
 }
 const surveyReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.FETCH_SURVEYS:
             return {
                 ...state,
-                surveys: action.payload
+                surveys: action.payload,
+                currentSurvey: null
             }
 
+        case actionTypes.SET_CURRENT_SURVEY: 
+            return {
+                ...state,
+                currentSurvey: action.survey
+            }
+            
         default:
             return state;
     }
