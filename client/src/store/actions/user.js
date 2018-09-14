fb1eb87feb3a66095ad45f3c828b7c729b4001c1
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-14 20:11:03 +0530

    user action creators moved to separate file

---
+++ client/src/store/actions/user.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import * as actionTypes from './types';
+
+export const fetchUser = (data) => {
+    return {
+        type: actionTypes.FETCH_USER,
+        payload: data
+    }
+}
+
+export const submitSurvey = (values, history) => {
+    return async dispatch => {
+        const res = await axios.post('/api/surveys', values);
+        
+        history.push('/surveys');
+        dispatch(fetchUser(res.data));
+    }
+};
+
+export const fetchApi = () => {
+    return async dispatch => {
+        const res = await axios.get('/api/current_user');
+        dispatch(fetchUser(res.data));
+    }
+}
+
+export const handleToken = (token) => {
+    return async dispatch => {
+        const res = await axios.post('/api/stripe', token);
+        dispatch(fetchUser(res.data));
+    }
+}
