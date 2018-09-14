d6799c32c7d7c692e1b267a58fbb619c3be9cf32
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-14 20:12:08 +0530

    added new actionType

--- client/src/store/actions/types.js
+++ client/src/store/actions/types.js
@@ -1,2 +1,3 @@
-export const FETCH_USER = 'FETCH_USERS';
-export const FETCH_SURVEYS = 'FETCH_SURVEYS';
+export const FETCH_USER = 'FETCH_USERS'
+export const FETCH_SURVEYS = 'FETCH_SURVEYS'
+export const SET_CURRENT_SURVEY = 'SET_CURRENT_SURVEY'
